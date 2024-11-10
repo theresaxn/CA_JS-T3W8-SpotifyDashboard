d1a9e237d52106d6230b3e4c15cd5b5a4ff9ba31
theresaxn <hongoanh3@hotmail.com>
2024-11-10 19:02:12 +1100

    feat: project init

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import reactRefresh from 'eslint-plugin-react-refresh' 
 
 export default [
   { ignores: ['dist'] },
