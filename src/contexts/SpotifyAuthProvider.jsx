7c54bd8293f010ee3ee85cb103b479ea1aad9b98
theresaxn <hongoanh3@hotmail.com>
2024-11-10 19:15:29 +1100

    feat: correct callback url in deployment

--- src/contexts/SpotifyAuthProvider.jsx
+++ src/contexts/SpotifyAuthProvider.jsx
@@ -90,7 +90,7 @@ export function SpotifyAuthProvider({children}) {
 		const params = new URLSearchParams();
 		params.append("client_id", clientId);
 		params.append("response_type", "code");
-		params.append("redirect_uri", "http://localhost:5173/spotifycallback");
+		params.append("redirect_uri", import.meta.env.VITE_SPOTIFY_CALLBACK);
 		params.append("scope", "user-top-read user-read-private user-read-email");
 		params.append("code_challenge_method", "S256");
 		params.append("code_challenge", challenge);
